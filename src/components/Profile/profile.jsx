76d1d801cab6d9318a225109627acbb94aa49701
Oksana Chorna <ksuchorna@gmail.com>
2021-04-20 22:10:27 +0300

    Профіль стилі зроблено

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// eslint-disable-next-line no-unused-vars
+import styles from './Profile.module.css';
+console.log(styles);
 
 const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="Аватар пользователя" className="avatar" />
-        <p className="name">{name}</p>
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img className={styles.avatar} src={avatar} alt="Аватар пользователя" />
+        <p className={styles.name}>{name}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+      <ul className={styles.stats}>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
         </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+        <li className={styles.statsItem}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
