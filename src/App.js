d90d4693710935414d8c4af5f3af997c118cef49
Oksana Chorna <ksuchorna@gmail.com>
2021-04-20 14:11:46 +0300

    2ге крім проптипів

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import Profile from './components/profile';
+import React from 'react';
+import Profile from './components/Profile/profile';
 import user from './user.json';
+import Statistics from './components/Statistics/statistics';
+import statisticalData from './statistical-data.json';
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={statisticalData} />;
     </div>
   );
 }
